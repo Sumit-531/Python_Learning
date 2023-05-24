70a52ca563b4758c98fe90a22d5f13bc3a8795c0
Sumit-531 <sumitchakraborty218@gmail.com>
2023-05-25 04:24:41 +0600

    Day 1 coding practice

---
+++ Day_1/coding_practice/main.py
@@ -0,0 +1,8 @@
+# This prints the length of the input name.
+print(len(input("What is your name? ")))
+
+# Creating digit variable and assigning 12345
+digit = 12345
+
+# This prints the length of string
+print(len(str(56575999)))
