5d95546e85551fa3a645ca340e9e1c3da6a76937
Sumit-531 <sumitchakraborty218@gmail.com>
2023-05-25 04:37:04 +0600

    coding practice

---
+++ Day_2/coding_practice/main.py
@@ -0,0 +1,54 @@
+print("Hello"[0])
+print("Hello"[3])
+print(123_456_999 + 45_898)
+print(3.14159)
+
+num_char = len(input("What is your name?\n"))
+
+print(type(num_char))
+new_num_char = str(num_char)
+print("My name is: "+ new_num_char +" characters")
+
+print(int(8/3))
+print(round(8/3))
+print(8/3)
+print(round(2.6))
+print(round(8/3, 5))
+print(round(2.66666669, 4))
+print(type(8/3))
+
+result = 4/2
+result /= 2
+print(result)
+
+score = 100
+is_winning = True
+
+print("Your score is " +str(score) + " and winning status is " + str(is_winning))
+print(f"Your score is {score} and winning status is {is_winning}")
+
+tip = 1568 * 1.12
+print (round(tip,4))
+print(tip)
+number = 2.867053
+bill = "{:.3f}".format(number)
+print(bill)
+
+
+a = str(123)
+print(type(a))
+print(70 + float("170.5"))
+
+a = "170.5"
+b = float("170.5")
+print(30 + b)
+
+user_input = input("Type anything you want")
+
+print(1+2)
+print(5-2)
+print(3*1)
+print(6/2)
+
+print(1.75 * 1.75)
+print(1.75 ** 2)
