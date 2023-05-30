136aed9810523dce0c24d0350e1b521eac96a4c1
Sumit-531 <sumitchakraborty218@gmail.com>
2023-05-30 23:42:30 +0600

    Coding Practice

---
+++ Day_6/coding-practice/main.py
@@ -0,0 +1,18 @@
+def my_function():
+    print("Hello")
+    print("bye")
+
+
+my_function()
+number = 0
+for n in range(5):
+    print("Hello")
+
+number = 0
+number_to_print = 10
+while number_to_print > 0:
+    number += 1
+    print(number)
+    number_to_print -= 1
+
+
